dec21b2ff55ec21fa546a0eecf54c928c0430623
unknown <Administrator@virt-w2008-hcs.(none)>
2012-08-21 16:39:51 +0100

    basic emacs config

---
+++ .emacs.d/elpa/smex-1.1.2/smex-autoloads.el
@@ -0,0 +1,27 @@
+;;; smex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (smex-initialize) "smex" "smex.el" (20531 37412))
+;;; Generated autoloads from smex.el
+
+(autoload 'smex-initialize "smex" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("smex-pkg.el") (20531 37412 243000))
+
+;;;***
+
+(provide 'smex-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smex-autoloads.el ends here
---
+++ .emacs.d/init.el
@@ -0,0 +1,44 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      starter-kit-ruby
+                      starter-kit-js
+                      sr-speedbar
+                      handlebars-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; sr-speedbar
+
+(require 'sr-speedbar)
+(load "sr-speedbar")
+;; don't use icons
+(setq speedbar-use-images nil)
+;; show every file
+(setq speedbar-show-unknown-files t)
+;; show speedbar on the right side of the current window
+(setq sr-speedbar-right-side nil)
+;; skip speedbar when changing the active window
+(setq sr-speedbar-skip-other-window-p t)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/")
+
+;; use forked js2-mode
+;; https://github.com/mooz/js2-mode
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(global-set-key (kbd "<ESC> <f1>") 'sr-speedbar-toggle)
